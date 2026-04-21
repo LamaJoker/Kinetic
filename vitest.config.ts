447c3784501b45231c34ef0d0a0acf40a95158ff
LamaJoker <darkshadow25000@gmail.com>
2026-04-21 19:50:01 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html', 'json-summary'],
+      reporter: ['text', 'html', 'json', 'json-summary'], // 'json' ajouté ici
       reportsDirectory: 'tests/coverage',
       thresholds: {
         lines: 80,
